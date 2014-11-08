38ebc2856e94b02e9e73030a8baf5a100d4765aa
Nick Dyer <nbdyer@gmail.com>
2014-11-08 07:44:30 +0000

    Iterative inject working

--- lib/inject.rb
+++ lib/inject.rb
@@ -1,7 +1,41 @@
-class ArrayInject < Array
+class Array
 
-  def newinject(array)
+  def itrinject(*args)
+    if args.first.is_a?(Symbol) 
+      tempvalue = self.first 
+      self.shift
+      self.each { |item| tempvalue = tempvalue.send(args.first, item) }
+    elsif args[1].is_a?(Symbol) then
+      tempvalue = args.first
+      self.each { |item| tempvalue = tempvalue.send(args[1], item) }
+    elsif args.empty? then
+      tempvalue = self.first
+      self.shift
+      self.each { |n| tempvalue = yield tempvalue, n }
+    elsif args.count == 1 && !args.first.is_a?(Symbol) then
+      tempvalue = args.first
+      self.each { |n| tempvalue = yield tempvalue, n }
+    end
+    tempvalue
   end
 
+  def recinject(*args)
+    if args.first.is_a?(Symbol) 
+      tempvalue = self.first 
+      self.shift
+      self.each { |item| tempvalue = tempvalue.send(args.first, item) }
+    elsif args[1].is_a?(Symbol) then
+      tempvalue = args.first
+      self.each { |item| tempvalue = tempvalue.send(args[1], item) }
+    elsif args.empty? then
+      tempvalue = self.first
+      self.shift
+      self.each { |n| tempvalue = yield tempvalue, n }
+    elsif args.count == 1 && !args.first.is_a?(Symbol) then
+      tempvalue = args.first
+      self.each { |n| tempvalue = yield tempvalue, n }
+    end
+    tempvalue
+  end
 
 end
--- spec/inject_spec.rb
+++ spec/inject_spec.rb
@@ -1,9 +1,99 @@
 require 'inject'
 
-describe "ArrayInject" do
+describe "Array" do
 
-  it "should take an array as an input" do
+  let(:arr) {[2,3,4,5]}
 
+  context "method Iterative Inject" do
+
+    it "should compute addition as symbol" do
+      expect(arr.itrinject(:+)).to eq 14
+    end
+
+    it "should compute multiplication as symbol" do
+      expect(arr.itrinject(:*)).to eq 120
+    end
+
+     it "should compute subtraction as symbol" do
+      expect(arr.itrinject(:-)).to eq -10
+    end
+
+    it "should compute addition with intial value and symbol" do
+      expect(arr.itrinject(2, :+)).to eq 16
+    end
+
+    it "should compute multiplication with intial value and symbol" do
+      expect(arr.itrinject(2, :*)).to eq 240
+    end
+
+    it "should compute addition as a block" do
+      expect(arr.itrinject {|sum, n| sum + n }).to eq 14
+    end
+
+    it "should compute multiplication as a block" do
+      expect(arr.itrinject {|product, n| product * n }).to eq 120
+    end
+
+     it "should compute addition as a block with an initial value" do
+      expect(arr.itrinject(2) {|sum, n| sum + n }).to eq 16
+    end
+
+    it "should compute multiplication as a block with an initial value" do
+      expect(arr.itrinject(2) {|product, n| product * n }).to eq 240
+    end
+
+    it "should find the longest word" do
+      longest = %w{ cat sheep bear }
+      ans = longest.itrinject { |memo, word| memo.length > word.length ? memo : word }
+      expect(ans).to eq "sheep"      
+    end
+
+  end
+
+    context "method Recursive Inject" do
+
+    it "should compute addition as symbol" do
+      expect(arr.recinject(:+)).to eq 14
+    end
+
+    it "should compute multiplication as symbol" do
+      expect(arr.recinject(:*)).to eq 120
+    end
+
+     it "should compute subtraction as symbol" do
+      expect(arr.recinject(:-)).to eq -10
+    end
+
+    it "should compute addition with intial value and symbol" do
+      expect(arr.recinject(2, :+)).to eq 16
+    end
+
+    it "should compute multiplication with intial value and symbol" do
+      expect(arr.recinject(2, :*)).to eq 240
+    end
+
+    it "should compute addition as a block" do
+      expect(arr.recinject {|sum, n| sum + n }).to eq 14
+    end
+
+    it "should compute multiplication as a block" do
+      expect(arr.recinject {|product, n| product * n }).to eq 120
+    end
+
+     it "should compute addition as a block with an initial value" do
+      expect(arr.recinject(2) {|sum, n| sum + n }).to eq 16
+    end
+
+    it "should compute multiplication as a block with an initial value" do
+      expect(arr.recinject(2) {|product, n| product * n }).to eq 240
+    end
+
+    it "should find the longest word" do
+      longest = %w{ cat sheep bear }
+      ans = longest.recinject { |memo, word| memo.length > word.length ? memo : word }
+      expect(ans).to eq "sheep"      
+    end
+    
   end
 
 end
